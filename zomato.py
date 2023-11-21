2ab4ddde230547f2bd0e23977d18aefdc83c423e
Rahul Kumar Tiwari <134830578+tiwaririksh92@users.noreply.github.com>
2023-11-21 09:47:04 +0530

    Add files via upload

---
+++ zomato.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd       #zomato
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# In[2]:
+
+
+df = pd.read_csv('Zomato.csv')
+df
+
+
+# In[ ]:
+
+
+
+
